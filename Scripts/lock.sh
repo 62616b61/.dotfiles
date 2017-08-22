f52c5b84a8f4b4b8da4740b613e74949fa43decf
whynotcrybot <alexanderkatsero@gmail.com>
2017-08-22 19:07:24 +0300

    removed old files

--- Scripts/lock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-overlay="$HOME/Pictures/Lockscreen/lock_speech.png"
-tmpbg='/tmp/screen.png'
-
-scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% -colorize 25% "$tmpbg"
-convert "$tmpbg" "$overlay" -gravity south -composite -matte "$tmpbg"
-i3lock -e -i "$tmpbg"
