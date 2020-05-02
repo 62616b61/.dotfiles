0b7ffcaad1f40f89d589dd74dcc36fe2cf4d57f4
whynotcrybot <alexanderkatsero@gmail.com>
2020-05-02 19:16:57 +0300

    Reconfig polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env sh
 
+## Add this to your wm startup file.
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
-  done
-else
-  polybar --reload example &
-fi
-
-echo "Bars launched..."
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config main &
